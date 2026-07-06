531d7d40ea2efd9c3361600ecfe0025d2286f1ac
prutxvi <pruthviraj73962@gmail.com>
2026-07-07 00:05:02 +0530

    chore: add type hints to draw_text

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@
 import cv2
 
 
-def draw_text(img, text, pos, scale=0.6, color=(200, 200, 200), thickness=1, font=None):
+def draw_text(img, text, pos, scale: float = 0.6, color: tuple = (200, 200, 200), thickness: int = 1, font: int = None) -> None:
     font = font or cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(img, text, pos, font, scale, color, thickness)
