053ea0f6ddb9a81462e609dc271c301d2127a44d
prutxvi <pruthviraj73962@gmail.com>
2026-07-06 23:51:26 +0530

    refactor: add font parameter to draw_text

--- utils.py
+++ utils.py
@@ -2,5 +2,6 @@
 import cv2
 
 
-def draw_text(img, text, pos, scale=0.6, color=(200, 200, 200), thickness=1):
-    cv2.putText(img, text, pos, cv2.FONT_HERSHEY_SIMPLEX, scale, color, thickness)
+def draw_text(img, text, pos, scale=0.6, color=(200, 200, 200), thickness=1, font=None):
+    font = font or cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, text, pos, font, scale, color, thickness)
